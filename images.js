7b15f21bfa37112532b04551978fa82e9b53241d
BitFlipper99 <icemasta99@gmail.com>
2017-05-20 23:33:23 -0500

    Populate with Cities + catgif

--- images.js
+++ images.js
@@ -13,11 +13,27 @@ var images = [
 	"female_farmer",
 	"hemp",
 	"male_farmer",
-	"sugarcane"
+	"sugarcane",
+	"cloud",
 ];
 
+var gifs = [
+	"farmercat"
+]
+
+for (var i = 0; i < gifs.length; i++) {
+	var new_image = document.createElement("IMG");
+	new_image.src = "res/images/" + gifs[i] + ".gif";
+	new_image.id = gifs[i];
+	new_image.style.display = "none";
+	new_image.style.position = "fixed";
+	document.body.appendChild(new_image);
+}
+
+
+
 /* Initialize images */
-/*
+
 for (var i = 0; i < images.length; i++) {
 	var new_image = document.createElement("IMG");
 	new_image.src = "res/images/" + images[i] + ".png";
@@ -26,48 +42,83 @@ for (var i = 0; i < images.length; i++) {
 	new_image.style.position = "fixed";
 	document.body.appendChild(new_image);
 }
-*/
 
+
+var guatheight = 1600;
+var guatwidth = 1348;
 /* Initialize circles */
 var circles = [
-	{ color: "red", x: 250, y: 250, size: 100 },
-	{ color: "blue", x: 500, y: 250, size: 100 },
-	{ color: "green", x: 750, y: 250, size: 100 }
+	{ color: "red", x: 750/1348*guatwidth, y: 400/1600*guatheight, size: 30, id: "Flores"},
+	{ color: "red", x: 800/1348*guatwidth, y: 300/1600*guatheight, size: 30, id: "Tikal" },
+	{ color: "red", x: 325/1348*guatwidth, y: 835/1600*guatheight, size: 30, id: "Huehuetenango"},
+	{ color: "red", x: 630/1348*guatwidth, y: 800/1600*guatheight, size: 30, id: "Cobán"},
+	{ color: "red", x: 690/1348*guatwidth, y: 810/1600*guatheight, size: 30, id: "San Pedro Carchá"},
+	{ color: "red", x: 610/1348*guatwidth, y: 1080/1600*guatheight, size: 30, id: "La Ciudad de Guatemala"},
+	{ color: "red", x: 360/1348*guatwidth, y: 1120/1600*guatheight, size: 30, id: "Mazatenango"},
+	{ color: "red", x: 540/1348*guatwidth, y: 1280/1600*guatheight, size: 30, id: "San José"},
+	{ color: "red", x: 1050/1348*guatwidth, y: 750/1600*guatheight, size: 30, id: "Livingston"}
 ];
 for (var i = 0; i < circles.length; i++) {
-	var new_circle = document.createElement("DIV");
+	var new_circle = document.createElement("BUTTON");
 	new_circle.style.position = "fixed";
 	new_circle.style.display = "block";
 	new_circle.style.height = circles[i].size + "px";
 	new_circle.style.width = circles[i].size + "px";
 	new_circle.style.borderRadius = circles[i].size / 2;
-	new_circle.style.top = circles[i].x + "px";
-	new_circle.style.left = circles[i].y + "px";
+	new_circle.style.top = circles[i].y + "px";
+	new_circle.style.left = circles[i].x + "px";
+	new_circle.id = circles[i].id;
 	new_circle.style.backgroundColor = circles[i].color;
+	new_circle.style.borderRadius = "50%";
+	new_circle.style.display = "none";
 	document.body.appendChild(new_circle);
 }
 
-function showImage(imgId, height, width, x, y) {
+function showImage(imgId, height, width, y, x) {
 
 	var image = document.getElementById(imgId);
 
 	image.width = width;
 	image.height = height;
-	image.style.top = x+"px";
-	image.style.left = y+"px";
+	image.style.top = y+"px";
+	image.style.left = x+"px";
 
 	image.style.display = "inline";
 
 }
-//showImage("banana", 100, 100, 200, 200)
-//showImage("guatemala", 800*2, 674*2, 0, 0)
 
+//showImage("guatemala", guatheight, guatwidth, 0, 0)
 
-
-function moveImage(imgId, height, width, x1, y1, x2, y2, time){
+function showRepeating(imgId, height, width, x, y){
 
 	var image = document.getElementById(imgId);
 
-	//TODO
+}
+
+/*
+for (var i = 0; i < circles.length; i++){
+	var town = document.getElementById(circles[i].id);
+	town.style.display = "inline";
+}
+*/
+
+
+
+
+
+
+function begingame(){
+
+	showImage("farmercat", 404*1.5, 720*1.5, 160, 0);
+	var intro = document.createElement("DIV");
+	intro.style.color = "blue";
+	intro.style.fontSize = "150px";
+	intro.innerHTML = "Bienvenido!";
+	intro.style.left = "100px";
+	intro.style.position = "fixed";
+	document.body.appendChild(intro);
+
 
 }
+
+begingame();
